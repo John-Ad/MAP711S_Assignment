46bfc155e152f93ee5c50e5ceb04ffe7943b2d00
John-Ad <adriaanscorne@gmail.com>
2022-05-04 17:44:14 +0200

    converted to android x. added base navigation panel. got jobs fragment working again

--- app/src/main/java/com/example/valentines_garage/MainActivity.kt
+++ app/src/main/java/com/example/valentines_garage/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.valentines_garage
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentTransaction
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 
 class MainActivity : AppCompatActivity() {
