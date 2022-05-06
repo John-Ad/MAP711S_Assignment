50f2c11384aa77e7dd775bf4d3f8696ac085d1d6
John-Ad <adriaanscorne@gmail.com>
2022-05-06 17:02:46 +0200

    finished adding drawer and making it functional

--- app/src/main/java/com/example/valentines_garage/MainActivity.kt
+++ app/src/main/java/com/example/valentines_garage/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.valentines_garage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,9 +21,58 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val drawer: DrawerLayout = findViewById(R.id.drwr_main)
+        val toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.nav_open_drawer,
+            R.string.nav_close_drawer
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
+        navigationView.setNavigationItemSelectedListener(this)
+
         val fragment: Fragment = JobsFragment()
         val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
         ft.add(R.id.content_frame, fragment)
         ft.commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        var fragment: Fragment? = null
+
+        /*
+        * TODO: update user and reports fragments when implemented
+        * */
+        when (id) {
+            R.id.nav_jobs -> fragment = JobsFragment()
+            R.id.nav_users -> fragment = JobsFragment()
+            R.id.nav_reports -> fragment = JobsFragment()
+        }
+
+        if (fragment != null) {
+            val ft = supportFragmentManager.beginTransaction()
+            ft.replace(R.id.content_frame, fragment)
+            ft.commit()
+
+            val drawer: DrawerLayout = findViewById(R.id.drwr_main)
+            drawer.closeDrawer(GravityCompat.START)
+            return true
+        }
+
+        return false
+    }
+
+    override fun onBackPressed() {
+        val drawer: DrawerLayout = findViewById(R.id.drwr_main)
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
