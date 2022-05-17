679659e59d225d1b6471b9bc1581c46fc767c145
John-Ad <adriaanscorne@gmail.com>
2022-05-17 13:57:26 +0200

    finished user report view

--- app/src/main/java/com/example/valentines_garage/report_fragments/UserReportFragment.kt
+++ app/src/main/java/com/example/valentines_garage/report_fragments/UserReportFragment.kt
@@ -11,7 +11,9 @@ import com.example.valentines_garage.R
 import com.google.gson.JsonObject
 import com.valentines.connection.APIClient
 import com.valentines.connection.APIInterface
+import com.valentines.connection.adapters.TasksListAdapter
 import com.valentines.connection.models.Employee
+import com.valentines.connection.models.Task
 import com.valentines.connection.models.UserOverview
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,6 +23,7 @@ class UserReportFragment : Fragment() {
 
     private var tasks: ArrayList<String> = ArrayList()
     private var username: String? = ""
+    private var timeLine: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,10 +41,42 @@ class UserReportFragment : Fragment() {
         super.onStart()
 
         init()
+        setSpinners()
     }
 
     private fun init() {
         getOverviewDetails()
+        getTasksData()
+    }
+
+    private fun setSpinners() {
+        // set timeline choice spinner
+        val adapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            arrayListOf<String>("Day", "Month")
+        )
+        requireView().findViewById<Spinner>(R.id.spnnr_report_date).adapter = adapter
+
+        requireView().findViewById<Spinner>(R.id.spnnr_report_date).onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    timeLine = position
+                    getOverviewDetails()
+                    getTasksData()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    parent!!.setSelection(0)
+                }
+
+            }
+
     }
 
     //----   GET OVERVIEW DETAILS   ----
@@ -58,7 +93,9 @@ class UserReportFragment : Fragment() {
         // get data from server
         val apiInterface: APIInterface = APIClient.getInstance().create(APIInterface::class.java)
         val call1: Call<UserOverview> =
-            apiInterface.getTaskReportForEmployeeByMonth(jsonObject.toString())
+            if (this.timeLine == 0) apiInterface.getTaskReportForEmployeeByMonth(jsonObject.toString()) else apiInterface.getTaskReportForEmployeeByDay(
+                jsonObject.toString()
+            )
 
         call1.enqueue(object : Callback<UserOverview> {
             override fun onResponse(
@@ -86,6 +123,52 @@ class UserReportFragment : Fragment() {
         })
     }
 
+    //----   GET TASKS DATA   ----
+    private fun getTasksData() {
+        // show that data is loading
+        setLoading(true)
+
+        // create data to send to with request
+        var jsonData: JsonObject = JsonObject()
+        jsonData.addProperty("username", this.username)
+
+
+        // get data from server
+        val apiInterface: APIInterface = APIClient.getInstance().create(APIInterface::class.java)
+        val call: Call<MutableList<Task>> =
+            if (this.timeLine == 0) apiInterface.getCompleteTasksForEmployeeByDay(jsonData.toString()) else apiInterface.getCompleteTasksForEmployeeByMonth(
+                jsonData.toString()
+            )
+
+        call.enqueue(object : Callback<MutableList<Task>> {
+            override fun onResponse(
+                call: Call<MutableList<Task>>,
+                response: Response<MutableList<Task>>
+            ) {
+                var tasks: MutableList<Task>? = response.body()
+
+                if (tasks != null) {
+                    setListViewData(tasks)
+                } else {
+                    showToast(
+                        "success but failure: " + response.message()
+                    )
+                }
+
+                setLoading(false)
+            }
+
+            override fun onFailure(call: Call<MutableList<Task>>, t: Throwable) {
+                setLoading(false)
+                Log.v("CALL: ", call.request().url().toString())
+                Log.e("RESPONSE: ", t.toString())
+                showToast("Failed to load data, try again later: " + t.message)
+                call.cancel()
+            }
+
+        })
+    }
+
     //----   SET LOADING   ----
     private fun setLoading(loading: Boolean) {
         val view: View? = view  // property access of getView()
@@ -95,20 +178,29 @@ class UserReportFragment : Fragment() {
     }
 
     //----   SET LISTVIEW DATA   ----
-    private fun setListViewData(data: MutableList<String>) {
-        var adapter =
-            ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, data)
-        val listView: ListView = requireView().findViewById(R.id.list_reports)
+    private fun setListViewData(data: MutableList<Task>) {
+        var adapter = TasksListAdapter(requireContext(), R.layout.task_record, data)
+        val listView: ListView = requireView().findViewById(R.id.list_reports_tasks)
         listView.adapter = adapter
     }
 
 
     //----   SET OVERVIEW DATA   ----
     private fun setOverviewData(data: UserOverview) {
+        requireView().findViewById<TextView>(R.id.txt_report_username).text =
+            "Report for " + this.username
         requireView().findViewById<TextView>(R.id.txt_report_completed_tasks).text =
-            "Completed Tasks: " + data.completedTasks
+            data.completedTasks
         requireView().findViewById<TextView>(R.id.txt_report_incomplete_tasks).text =
-            "Incomplete Tasks: " + data.incompleteTasks
+            data.incompleteTasks
+        requireView().findViewById<TextView>(R.id.txt_report_jobs_completed).text =
+            data.jobsCompleted
+        requireView().findViewById<TextView>(R.id.txt_report_job_most_completed).text =
+            data.jobMostCompleted
+        requireView().findViewById<TextView>(R.id.txt_report_job_most_incomplete).text =
+            data.jobMostIncomplete
+        requireView().findViewById<TextView>(R.id.txt_report_client_most_completed).text =
+            data.clientMostComplete
     }
 
     //----   SHOW TOAST   ----
